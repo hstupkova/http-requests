87b329a7b665cc749cc3d00583c05e4899a11dc1
Hana Stupková <hstupkova@gmail.com>
2021-10-20 10:11:59 +0200

    alternative syntax added

--- src/App.js
+++ src/App.js
@@ -42,19 +42,31 @@ function App() {
   }, [fetchMoviesHandler]);
 
   /* syntax using async / await
-  async function fetchMoviesHandler() {
-    const response = await fetch('https://swapi.dev/api/films');
-    const data = await response.json();
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      };
-    });
-    setMovies(transformedMovies);
-  }
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
   */
 
   return (
